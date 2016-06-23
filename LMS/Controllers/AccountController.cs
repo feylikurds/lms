1d9ff0b8bc119dd020264e140103ab68cfcb451d
Aryo Pehlewan <aryopehlewan@hotmail.com>
2016-06-23 16:41:48 +0200

    Fixed merge

--- LMS/Controllers/AccountController.cs
+++ LMS/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using LMS.Models;
+using System.Collections.Generic;
 
 namespace LMS.Controllers
 {
