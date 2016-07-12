afa4c99decc13b962fef00cdc669cd8789948e52
Axel Räntilä <Axelrantila@gmail.com>
2016-07-12 08:39:11 +0200

    Fixed sorting

--- LMS/Controllers/StudentActivitiesController.cs
+++ LMS/Controllers/StudentActivitiesController.cs
@@ -35,7 +35,11 @@ namespace LMS.Controllers
                             select u).FirstOrDefault();
                 courses = (from c in db.Courses
                           where c.Id == user.CourseId
-                          select c).ToList(); 
+                          select c)
+                          .OrderBy(c => c.StartDate)
+                          .ThenBy(c => c.EndDate)
+                          .ThenBy(c => c.Name)
+                          .ToList(); 
             }
 
             return View(courses);
